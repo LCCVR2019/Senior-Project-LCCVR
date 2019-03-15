d397fe4c6c255d5f82bfae812da0471c9c70d78b
Thikrayat <th.sami95@hotmail.com>
2019-03-15 20:37:18 +0300

    Practical Part

---
+++ Assets/_Motherboard/Scripts/PartTracker.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartTracker : MonoBehaviour
+{
+    public static PartTracker Instance;
+
+    public Part m_CurrentPart = null;
+
+    private void Awake()
+    {
+        Instance = this;
+
+        PlayerEvents.OnBack += BackReturn;
+    }
+
+    private void OnDestroy()
+    {
+        PlayerEvents.OnBack -= BackReturn;
+    }
+
+    public bool HasPart()
+    {
+        if (m_CurrentPart) return true;
+        else return false;
+    }
+
+    public void SetCurrentPart(Part part)
+    {
+        if (m_CurrentPart != null) m_CurrentPart.Return();
+        m_CurrentPart = part;
+    }
+
+    private void BackReturn()
+    {
+        if (m_CurrentPart != null) m_CurrentPart.Return();
+        m_CurrentPart = null;
+    }
+}
